86f9e69d1b0b331b41572600687c7eb05c3f764a
Dibson T Hoffweiler <consulting@hoffweiler.com>
2010-12-01 17:16:35 -0800

    Make a TicTacToe module, with class Board to manage filling board elements.  At this point, just shuffling Greg's code.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,34 +1,64 @@
-board   = [[nil,nil,nil],
-           [nil,nil,nil],
-           [nil,nil,nil]]
+module TicTacToe
+  class OutOfBoundsError < StandardError; end;
+  class CellOccupiedError < StandardError; end;
+
+  class Board
+
+    attr_reader :cells
+
+    def initialize()
+      @cells = [[nil,nil,nil],
+                [nil,nil,nil],
+                [nil,nil,nil]]
+    end
+
+    def fill_cell(row, col, token)
+
+      begin
+        cell_contents = @cells.fetch(row).fetch(col)
+      rescue IndexError
+        raise OutOfBoundsError
+      end
+
+      if cell_contents
+        raise CellOccupiedError
+      end
+
+      @cells[row][col] = token
+
+    end
+
+    def to_s
+      @cells.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
+    end
+
+  end
+end
+
+board   = TicTacToe::Board.new
 
 left_diagonal = [[0,0],[1,1],[2,2]]
 right_diagonal = [[2,0],[1,1],[0,2]]
-
 
 players = [:X, :O].cycle
 
 current_player = players.next 
 
 loop do
-  puts board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
+  puts board
   print "\n #{current_player} >> "
   row, col = gets.split.map { |e| e.to_i }
   puts
 
   begin
-    cell_contents = board.fetch(row).fetch(col)
-  rescue IndexError
-    puts "Out of bounds, try another position"
+    board.fill_cell(row, col, current_player)
+  rescue TicTacToe::OutOfBoundsError
+    puts "Row/Col are out of bounds"
+    next
+  rescue TicTacToe::CellOccupiedError
+    puts "Cell is occupied"
     next
   end
-  
-  if cell_contents
-    puts "Cell occupied, try another position"
-    next
-  end
-
-  board[row][col] = current_player
 
   lines = []
 
@@ -40,7 +70,7 @@ loop do
   lines << (0..2).map { |r1| [r1, col] }
 
   win = lines.any? do |line|
-    line.all? { |row,col| board[row][col] == current_player }
+    line.all? { |row,col| board.cells[row][col] == current_player }
   end
 
   if win
@@ -48,7 +78,7 @@ loop do
     exit
   end
 
-  if board.flatten.compact.length == 9
+  if board.cells.flatten.compact.length == 9
     puts "It's a draw!"
     exit
   end
